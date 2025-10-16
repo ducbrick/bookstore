d1c87f5defa8eb526706cc7d6f1f1a90ec15f448
PhuHV <phuhv102@gmail.com>
2025-10-16 10:07:06 +0700

    Refactor

--- src/test/java/com/crni99/bookstore/controller/CartControllerTest.java
+++ src/test/java/com/crni99/bookstore/controller/CartControllerTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.ui.Model;
--- src/test/java/com/crni99/bookstore/service/ShoppingCartServiceTest.java
+++ src/test/java/com/crni99/bookstore/service/ShoppingCartServiceTest.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.Test;
 
