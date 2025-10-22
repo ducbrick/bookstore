2cb3e2d2cf371e39b8e44f91524ccfd502123308
ducbrick <ducbrick@gmail.com>
2025-10-22 22:12:16 +0700

    feat: add frontend module with no features

---
+++ frontend/src/main/java/com/ducbrick/bookstore_frontend/BookstoreFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ducbrick.bookstore_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreFrontendApplication.class, args);
+	}
+
+}
---
+++ frontend/src/test/java/com/ducbrick/bookstore_frontend/BookstoreFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducbrick.bookstore_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
