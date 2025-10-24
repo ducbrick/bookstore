286d582e7f61d205dabf8807ffa33a163e600c61
ducbrick <ducbrick@gmail.com>
2025-10-25 00:15:03 +0700

    feat: update backend cors config

--- backend/src/main/java/com/crni99/bookstore/security/SecurityConfig.java
+++ backend/src/main/java/com/crni99/bookstore/security/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.Arrays;
+
 @Configuration
 public class SecurityConfig {
 
@@ -40,7 +43,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-								.cors(cors -> cors.disable())
+								.cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Arrays.asList("http://localhost:8081"));
+                    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                    config.setAllowedHeaders(Arrays.asList("*"));
+                    config.setAllowCredentials(true);
+                    return config;
+                }))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
