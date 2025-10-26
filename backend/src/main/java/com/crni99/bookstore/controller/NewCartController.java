8d66b459d19713ffb6656aed44511ecb85f09ae3
nhuhieu193 <mama82bells@gmail.com>
2025-10-26 21:25:03 +0700

    cart.html

--- backend/src/main/java/com/crni99/bookstore/controller/NewCartController.java
+++ backend/src/main/java/com/crni99/bookstore/controller/NewCartController.java
@@ -3,11 +3,8 @@ package com.crni99.bookstore.controller;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.crni99.bookstore.service.BookService;
+import org.springframework.web.bind.annotation.*;
 
 import com.crni99.bookstore.model.Book;
 import com.crni99.bookstore.service.ShoppingCartService;
@@ -22,12 +19,28 @@ import lombok.extern.slf4j.Slf4j;
 public class NewCartController {
 
 	private final ShoppingCartService cartService;
+    private final BookService bookService;
 
 	@GetMapping
 	public List<Book> getCart() {
 		log.info("Get cart");
 		return cartService.getCart();
 	}
+
+    @PostMapping("/{id}")
+    public void addProductToCart(@PathVariable("id") Long bookId) {
+        log.info("Add product to cart with id {}", bookId);
+
+        Book book = bookService.findBookById(bookId).orElse(null);
+
+        if (book != null) {
+            List<Book> cart = cartService.getCart();
+            cart.add(book);
+            cartService.getSession().setAttribute("cart", cart);
+        } else {
+            log.warn("Attempted to add non-existent book with id {}", bookId);
+        }
+    }
 
 	@GetMapping("/total")
 	public BigDecimal getTotalPrice() {
